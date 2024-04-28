2472d63208d141be31a984f2a2f12e0b46e282c3
Will Burgess <112338817+will-brgs@users.noreply.github.com>
2024-04-28 12:30:39 -0500

    Add files via upload

---
+++ EnmathB_Final_Project.m
@@ -0,0 +1,7 @@
+%% Enmath B Final Project:
+%% Introduction
+
+% * Author:                   Will Burgess
+% * Class:                    ESE 319
+% * Date:                     Created 4/28/2024, Last Edited 5/6/2024
+%%
