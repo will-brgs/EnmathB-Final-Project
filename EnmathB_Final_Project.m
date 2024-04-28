0f90fb3c83ad69d2a483d174db13607b24f353c0
Will <Will@Will-laptop>
2024-04-28 16:09:37 -0500

    we chillin

--- EnmathB_Final_Project.m
+++ EnmathB_Final_Project.m
@@ -4,4 +4,59 @@
 % * Author:                   Will Burgess
 % * Class:                    ESE 319
 % * Date:                     Created 4/28/2024, Last Edited 5/6/2024
-%%
+%% Housekeeping
+close all
+clc
+clear
+filepath = "C:\Users\Will\OneDrive - Washington University in St. Louis\. EnMath B\Final Project\Figures";
+
+%% Objective 1: Find the Solution to the BVP
+% All relevant caclulations are done by hand and carried out in the
+% objective 2 calculation
+%% Objective 2: Evaluate the First Six Nonzero Terms of u(r,theta)
+
+%independent Vars
+numvals = 100;
+theta = linspace(0,2*pi,numvals); % Covers entire sphere
+r = linspace(0,3,numvals); % diamater is 6cm
+outputMerged = zeros(numvals,numvals);
+
+[meshR, meshTheta] = meshgrid(r, theta);
+
+fig1 = figure();
+sgtitle('Contribution to Concentration for each Nonzero Index of N')
+for n = 0:5
+% Output Calculation for u(r,theta)
+figindex = n + 1;
+constant = (2.*n + 1)/(2.*(3).^n);
+integralIn = @(x) (1/6) .* besselk(0,sqrt((1+x)/2)) .* legendreP(n, x);
+integralOut = integral(integralIn,-1,1);
+outputTerm = (constant .* integralOut .* r.^n .* legendreP(n,cos(theta)));
+outputMerged = outputMerged + outputTerm;
+
+% Plotting Mesh Contributions (Omitting this for now, having issues with
+% dimension of OutputTerm being 1x100)
+
+% subplot(3,2,figindex)
+% Z = squeeze(outputTerm);
+% surf(meshR .* cos(meshTheta), meshR .* sin(meshTheta), Z, 'edgecolor','none');
+% xlabel('X Position (cm)'),ylabel('Y Position (cm)'),title(['n = ' num2str(n)])
+% colormap('jet');
+% bar = colorbar;
+% ylabel(bar, 'Quantity Concentration');
+% view(0,90);
+end
+
+%Merged Mesh Figure
+fig2 = figure();
+Z = squeeze(outputMerged);
+surf(meshR .* cos(meshTheta), meshR .* sin(meshTheta), Z, 'edgecolor','none');
+xlabel('X Position (cm)'),ylabel('Y Position (cm)'),title({'Quantity Concentration of a Uniform Slice of the Sphere' 'Merged Results of First 6 Nonzero Coefficients'})
+colormap('jet');
+bar = colorbar;
+ylabel(bar, 'Quantity Concentration');
+view(0,90);
+
+%% Export Figures
+exportgraphics(fig1, fullfile(filepath, 'Independent Output.jpg'), 'resolution', 300);
+exportgraphics(fig2, fullfile(filepath, 'Merged Output.jpg'), 'resolution', 300);
