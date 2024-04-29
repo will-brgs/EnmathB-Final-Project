3b8496845d284b651090f0340149768238e7fcff
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-04-29 15:07:48 -0500

    ughhhhhhhhhh

--- SphereCoordsAttempt.m
+++ SphereCoordsAttempt.m
@@ -3,7 +3,7 @@
 
 % * Author:                   Will Burgess
 % * Class:                    ESE 319
-% * Date:                     Created 4/28/2024, Last Edited 5/6/2024
+% * Date:                     Created 4/28/2024, Last Edited 
 %% Housekeeping
 close all
 clc
@@ -28,50 +28,60 @@ obj2_outputs(index) = integralOut * constant;
 end
 % Direct inspection of the obj2_outputs variable is applied to hand
 % calculations
+
 %% Objective 3: Genereate a Figure Displaying Concentration inside Sphere
+
 %independent Vars
 numvals = 100;
-theta = linspace(0,2*pi,numvals); % Covers entire sphere
-r = linspace(0,2,numvals); % diamater is 6cm
+theta = linspace(0,2*pi,numvals); % Covers entire sphere, use radians
+r = linspace(0,3,numvals); % diamater is 6cm
+phi = linspace(-1*pi, pi, numvals);
 outputMerged = zeros(numvals,numvals);
 
-[meshR, meshTheta] = meshgrid(r, theta);
-
-fig1 = figure();
-sgtitle('Contribution to Concentration for each Nonzero Index of N')
+% fig1 = figure();
+% sgtitle('Contribution to Concentration for each Nonzero Index of N')
 for n = 0:5
 % Output Calculation for u(r,theta)
 figindex = n + 1;
-constant = (2.*n + 1)/(2.^(n+1));
+constant = ((2.*n) + 1)/(2.^(n+1));
 integralIn = @(x) (1/6) .* besselk(0,sqrt((1+x)/2)) .* legendreP(n, x);
 integralOut = integral(integralIn,-1,1);
-outputTerm = constant .* integralOut .* r.^n;
-outputTerm = outputTerm.' * legendreP(n,cos(theta));
+outputTerm = (constant .* integralOut .* r.^n);
+
+%perform matrix multiplication to obtain unique range for each value of r & theta accross numvals
+outputTerm = (outputTerm.' * legendreP(n,cos(theta))); 
 outputMerged = outputMerged + outputTerm;
 
+% [meshR, meshTheta, meshPhi] = meshgrid(r,theta, phi);
+[x,y,z] = sph2cart(theta, phi, r);
+[meshX, meshY, meshZ] = meshgrid(x,y,z);
+% x = reshape(x, [], numvals);
+% y = reshape(y, [], numvals);
+% z = reshape(z, [], numvals);
+
 % Plotting Mesh Contributions
-
-subplot(3,2,figindex)
-Z = squeeze(outputTerm);
-surf(meshR .* cos(meshTheta), meshR .* sin(meshTheta), Z, 'edgecolor','none');
-xlabel('X Position (cm)'),ylabel('Y Position (cm)'),title(['n = ' num2str(n)])
-colormap('jet');
-bar = colorbar;
-caxis([-0.1, 0.14])
-ylabel(bar, 'Quantity Concentration');
-view(0,90);
-
+% subplot(3,2,figindex)
+% Z = squeeze(outputTerm);
+% surf(meshR .* cos(meshTheta), meshR .* sin(meshTheta), Z, 'edgecolor','none');
+% xlabel('X Position (cm)'),ylabel('Y Position (cm)'),title(['n = ' num2str(n)])
+% colormap('jet');
+% bar = colorbar;
+% caxis([-0.1, 0.14])
+% ylabel(bar, 'Quantity Concentration');
+% view(0,90);
+% grid off
 end
-
-%% Merged Mesh Figure
+%% Generate Merged Mesh Figure
 fig2 = figure();
-Z = squeeze(outputMerged);
-surf(meshR .* cos(meshTheta), meshR .* sin(meshTheta), Z, 'edgecolor','none');
+%Z = squeeze(outputMerged);
+surf(meshX, meshY, meshZ, outputMerged, 'edgecolor','none');
 xlabel('X Position (cm)'),ylabel('Y Position (cm)'),title({'Quantity Concentration of a Uniform Slice of the Sphere' 'Merged Results of First 6 Nonzero Coefficients'})
 colormap('jet');
 bar = colorbar;
 ylabel(bar, 'Quantity Concentration');
-view(0,90);
+grid off
+%view(0,90);
+
 
 %% Export Figures
 % exportgraphics(fig1, fullfile(filepath, 'Independent Output.jpg'), 'resolution', 300);
